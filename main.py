e1e09d70e080a3b9bb754ae11f8efdb6be45019a
SaintAugustus <saintaugustus2020@gmail.com>
2023-09-29 17:21:35 -0500

    add datasets

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def main(lr, num_epochs, device, batch_size, input_dim, d_model, ffn_dim,
                            num_layers=num_layers, dropout=dropout)
     total_params = sum(p.numel() for p in net.parameters())
     print(f"Number of parameters: {total_params}")
-    train(net, train_iter, valid_iter, test_iter, lr=lr, num_epochs=num_epochs,
-          device=torch.device(device))
+    train(net, train_iter, valid_iter, test_iter, lr=lr, num_epochs=num_epochs, device=torch.device(device))
 
 
 
